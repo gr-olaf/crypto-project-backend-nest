c4e50cb563aa15a8cbc2c2f7b674be25a5340c0e
Alexandr Zaycev <hunter290592@yandex.ru>
2023-04-13 19:36:56 +0300

    Refactoring refactoring_app - Improved auth service - Improved watchlist service - Improved user service

--- src/modules/watchlist/watchlist.service.ts
+++ src/modules/watchlist/watchlist.service.ts
@@ -12,19 +12,27 @@ export class WatchlistService {
    ) {}
 
    async createAsset(user, dto: WatchlistDTO): Promise<CreateAssetResponse> {
-      const watchlist = {
-         user: user.id,
-         name: dto.name,
-         assetId: dto.assetId,
-      };
-      await this.watchlistRepository.create(watchlist);
-      return watchlist;
+      try {
+         const watchlist = {
+            user: user.id,
+            name: dto.name,
+            assetId: dto.assetId,
+         };
+         await this.watchlistRepository.create(watchlist);
+         return watchlist;
+      } catch (error) {
+         throw new Error(error);
+      }
    }
 
    async deleteAsset(userId: number, assetId: string): Promise<boolean> {
-      await this.watchlistRepository.destroy({
-         where: { id: assetId, user: userId },
-      });
-      return true;
+      try {
+         await this.watchlistRepository.destroy({
+            where: { id: assetId, user: userId },
+         });
+         return true;
+      } catch (error) {
+         throw new Error(error);
+      }
    }
 }
